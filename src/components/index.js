349f81d7dcd075f061d11e504585475851b4c08b
Deerah1234 <chiderajames55@gmail.com>
2025-01-29 16:37:40 +0100

    minor fix

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Hero from "./Hero";
 import BackgroundVideo from "../assets/videos/heroVid.mp4";
 import BgVideo from "./BgVideo";
 import Heading from "./common/Heading";
-import Footer from "./footer";
+import Footer from "./Footer";
 import PageTitle from "./common/PageTitle";
 
 export {
